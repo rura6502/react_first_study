b6ba6c07e46f78511f1a3be6867fbb635e047993
rura6502 <rura6502@hotmail.com>
2022-07-24 08:51:08 +0900

    feat: css 적용

--- app.js
+++ app.js
@@ -22,38 +22,86 @@ function showContent() {
   
   const content = callApi(CONTENT_API.replace("@id", id));
 
-  app.innerHTML = `
-    <h1>${content.title}</h1>
-    <div>
-      <a href="#/page/${store.currentPage}">목록으로</a>
-    </div>
+  let template = `
+    <div class="bg-gray-600 min-h-screen pb-8">
+      <div class="bg-white text-xl">
+        <div class="mx-auto px-4">
+          <div class="flex justify-between items-center py-6">
+            <div class="flex justify-start">
+              <h1 class="font-extrabold">Hacker News</h1>
+            </div>
+            <div class="items-center justify-end">
+              <a href="#/page/${store.currentPage}" class="text-gray-500">
+                <i class="fa fa-times"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="h-full border rounded-xl bg-white m-6 p-4 ">
+        <h2>${content.title}</h2>
+        <div class="text-gray-400 h-20">
+          ${content.content}
+        </div>
+
+        {{__comments__}}
+
+      </div>
+    </div> 
   `;
+  app.innerHTML = template;
 }
+
 
 function showList() {
   const data = callApi(LIST_API)
   const titles = [];
 
   let template = `
-    <div>
-      <h1>Hacker News</h1>
-      <ul>
-        {{__titles__}}
-      </ul>
-      <div>
-        <a href="#/page/{{__previous_page__}}">이전</a>
-        <a href="#/page/{{__next_page__}}">다음</a>
+  <div class="bg-gray-600 min-h-screen">
+    <div class="bg-white text-xl">
+      <div class="mx-auto px-4">
+        <div class="flex justify-between items-center py-6">
+          <div class="flex justify-start">
+            <h1 class="font-extrabold">Hacker News</h1>
+          </div>
+          <div class="items-center justify-end">
+            <a href="#/page/{{__previous_page__}}" class="text-gray-500">
+              Previous
+            </a>
+            <a href="#/page/{{__next_page__}}" class="text-gray-500 ml-4">
+              Next
+            </a>
+          </div>
+        </div> 
       </div>
     </div>
+    <div class="p-4 text-2xl text-gray-700">
+      {{__titles__}}
+    </div>
+  </div>
   `;
 
   for (let i=(store.currentPage - 1) * 10; i< store.currentPage * 10; i++) {
     titles.push(`
-      <li>
-        <a href="#/show/${data[i].id}">
-          ${data[i].title} (${data[i].comments_count})
-        </a>
-      </li>
+      <div class="p-6 ${data[i].read ? 'bg-red-500' : 'bg-white'} mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
+      <div class="flex">
+        <div class="flex-auto">
+          <a href="#/show/${data[i].id}">${data[i].title}</a>  
+        </div>
+        <div class="text-center text-sm">
+          <div class="w-10 text-white bg-green-300 rounded-lg px-0 py-2">${data[i].comments_count}</div>
+        </div>
+      </div>
+      <div class="flex mt-3">
+        <div class="grid grid-cols-3 text-sm text-gray-500">
+          <div><i class="fas fa-user mr-1"></i>${data[i].user}</div>
+          <div><i class="fas fa-heart mr-1"></i>${data[i].points}</div>
+          <div><i class="far fa-clock mr-1"></i>${data[i].time_ago}</div>
+        </div>
+      </div>
+    </div>
     `);
   }
   
